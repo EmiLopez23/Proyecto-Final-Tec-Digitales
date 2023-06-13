d13837f659b8d01482549d911b2670fa118dece8
Emi <emiliano.lopez@ing.austral.edu.ar>
2023-06-13 14:14:09 -0300

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,100 @@
+/*
+ *  Project 34-websock - main.cpp
+ *      Show that using WebSockets is better than using normal Web servers
+ *      in near real time systems
+ *
+ *      Austral 2023 - Electronica Digital - EAM
+ */
+
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include <WebSocketsServer.h>
+#include "wifi_ruts.h"
+#include "webpage.h"
+
+#define PIN_1 14
+#define PIN_2 15
+#define PIN_3 16
+
+WebServer server;
+WebSocketsServer webSocket = WebSocketsServer(81);
+
+/*
+ *  Private functions
+ */
+
+void activatePin(int pin, int percentage) {
+  digitalWrite(pin, HIGH);
+  delay(10000*percentage/100);  // Multiplica por 10 para ajustar el rango de porcentaje a milisegundos
+  digitalWrite(pin, LOW);
+}
+
+void handleRoot() {
+  server.send(200, "text/html", webpage);
+}
+
+void fillWithDrink1() {
+  Serial.print("Drink 1");
+  digitalWrite(PIN_1,HIGH);
+  delay(10000);
+  digitalWrite(PIN_1,LOW);
+  server.send(200, "text/plain", "OK");
+}
+
+void fillWithDrink2() {
+  int drink2Percentage = server.arg("percentage").toInt();
+  Serial.print("Drink 2");
+  digitalWrite(PIN_2,HIGH);
+  delay(10000);
+  digitalWrite(PIN_2,LOW);
+  server.send(200, "text/plain", "OK");
+}
+
+void fillWithDrink3() {
+  int drink3Percentage = server.arg("percentage").toInt();
+  Serial.print("Drink 3");
+  digitalWrite(PIN_3,HIGH);
+  delay(10000);
+  digitalWrite(PIN_3,LOW);
+  server.send(200, "text/plain", "OK");
+}
+
+void handleDrinkPercentages(){
+  int drink1Percentage = server.pathArg(0).toInt();
+  int drink2Percentage = server.pathArg(2).toInt();
+  int drink3Percentage = server.pathArg(4).toInt();
+
+  Serial.print("Drink 1: ");
+  Serial.println(drink1Percentage);
+  activatePin(PIN_1, drink1Percentage);
+
+  Serial.print("Drink 2: ");
+  Serial.println(drink2Percentage);
+  activatePin(PIN_2, drink2Percentage);
+
+  Serial.print("Drink 3: ");
+  Serial.println(drink3Percentage);
+  activatePin(PIN_3, drink3Percentage);
+
+  server.send(200, "text/plain", "Drink percentages received");
+}
+
+void setup() {
+  Serial.begin(SERIAL_BAUD);
+
+  wifi_connect();
+
+  server.on("/", handleRoot);
+  server.on("/drink1", fillWithDrink1);
+  server.on("/drink2", fillWithDrink2);
+  server.on("/drink3", fillWithDrink3);
+  server.on("/drink1/<int:drink1>/drink2/<int:drink2>/drink3/<int:drink3>", handleDrinkPercentages);
+
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
+
---
+++ src/webpage.h
@@ -0,0 +1,122 @@
+char webpage[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Document</title>
+    <script>
+        
+        function fillDrink(event){
+          const id = event.target.id
+          console.log(`/drink${id}`)
+          fetch(`/drink${id}`)
+            .then(response=>{
+              if (response.ok) {
+                return response.text();
+              } else {
+                throw new Error(`Error en la solicitud para el ${id} Drink`);
+              }
+            })
+            .then(data => {
+              console.log(data);
+            })
+            .catch(error => {
+              console.error(error);
+            })
+        } 
+
+        
+
+        function submitValues() {
+          let drink1 = parseInt(document.getElementById('percentage-range1').value);
+          let drink2 = parseInt(document.getElementById('percentage-range2').value);
+          let drink3 = parseInt(document.getElementById('percentage-range3').value);
+          let result = drink1 + drink2 + drink3  
+
+          result==100 
+            ? mixValues(drink1,drink2,drink3)
+            : result > 100
+              ? console.log("this is more than 100%")
+              : console.log("must be 100%")
+                  
+        }
+
+        function mixValues(drink1,drink2,drink3){
+          fetch(`/drink/${drink1}/drink2/${drink2}/drink3/${drink3}`)
+          .then(response=>{
+              if (response.ok) {
+                return response.text();
+              } else {
+                throw new Error(`Error en la solicitud para el mix Drink`);
+              }
+            })
+            .then(data => {
+              console.log(data);
+            })
+            .catch(error => {
+              console.error(error);
+            })
+        }
+
+        function updateRangeValue() {
+          let range = document.getElementById('percentage-range');
+          let value = range.value;
+          let rangeValue = document.getElementById('range-value');
+          rangeValue.textContent = value + '%';
+        }
+        function updateRangeValue(drinkNumber) {
+          let range = document.getElementById('percentage-range' + drinkNumber);
+          let value = range.value;
+          let rangeValue = document.getElementById('range-value' + drinkNumber);
+          rangeValue.textContent = value + '%';
+        }
+      </script>
+</head>
+<body onload="javascript:init()" class="vh-100 bg-dark d-flex flex-column justify-content-center align-items-center gap-3">
+    <div class="d-flex justify-content-center align-items-center gap-3">
+        <div>
+            <button class="btn btn-secondary" id="1" onclick="fillDrink(event)">1st Drink</button>
+          </div>
+          <div>
+              <button class="btn btn-secondary" id="2" onclick="fillDrink(event)">2nd Drink</button>
+          </div>
+          <div>
+              <button class="btn btn-secondary" id="3" onclick="fillDrink(event)">3rd Drink</button>
+          </div>
+    </div>
+    <div class="d-flex flex-column align-items-center justify-content-center gap-3">
+        <h3 class="text-light">For a mix:</h3>
+        <div class="text-light">
+            <div>1st Drink:</div>
+            <div class="d-flex align-items-center justify-content-center gap-3">
+                <input type="range" id="percentage-range1" min="0" max="100" step="10" onchange="updateRangeValue(1)" oninput="updateRangeValue(1)">
+                <div id="range-value1" style="width: 30px;">0%</div>
+            </div>
+        
+          </div>
+        
+          <div class="text-light">
+            <div>2nd Drink:</div>
+            <div class="d-flex align-items-center justify-content-center gap-3">
+                <input type="range" id="percentage-range2" min="0" max="100" step="10" onchange="updateRangeValue(2)" oninput="updateRangeValue(2)">
+                <div id="range-value2" style="width: 30px;">0%</div>
+            </div>
+        
+          </div>
+        
+          <div class="text-light">
+            <div>3rd Drink:</div>
+            <div class="d-flex align-items-center justify-content-center gap-3">
+                <input type="range" id="percentage-range3" min="0" max="100" step="10" onchange="updateRangeValue(3)" oninput="updateRangeValue(3)">
+                <div id="range-value3" style="width: 30px;">0%</div>
+            </div> 
+          </div>
+          <button class="btn btn-success" onclick="submitValues()">Enviar</button>
+    </div>  
+    
+  </body>
+</html>
+)=====";
+
